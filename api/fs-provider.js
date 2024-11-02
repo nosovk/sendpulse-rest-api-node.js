b5d0e9e6617168b2d567a0e137561883e5348fb2
Daniyar <dstealord@gmail.com>
2024-11-02 06:42:16 +0500

    fix exports

--- api/fs-provider.js
+++ api/fs-provider.js
@@ -1,38 +1,39 @@
-var fs = require('node:fs');
+var fs = require("node:fs");
 /**
  * Create directory
  *
  * @param directory
  */
 function mkdirSyncRecursive(directory) {
-    var path = directory.replace(/\/$/, '').split('/');
-    for (var i = 1; i <= path.length; i++) {
-        var segment = path.slice(0, i).join('/');
-        segment.length > 0 && !fs.existsSync(segment) ? fs.mkdirSync(segment) : null;
-    }
-};
-
-class fsProvider {
-    constructor(path) {
-
-        if (!fs.existsSync(path)) {
-            mkdirSyncRecursive(path);
-        }
-        if (path.substr(-1) !== '/') {
-            path += '/';
-        }
-        this.path = path;
-    }
-    getToken(hashName) {
-        if (fs.existsSync(this.path + hashName)) {
-            return fs.readFileSync(this.path + hashName, {encoding: 'utf8'});
-        } else {
-            return "";
-        }
-    }
-    setToken(hashName, token) {
-        fs.writeFileSync(this.path + hashName, token);
-    }
+  var path = directory.replace(/\/$/, "").split("/");
+  for (var i = 1; i <= path.length; i++) {
+    var segment = path.slice(0, i).join("/");
+    segment.length > 0 && !fs.existsSync(segment)
+      ? fs.mkdirSync(segment)
+      : null;
+  }
 }
 
-module.exports = fsProvider;
+class fsProvider {
+  constructor(path) {
+    if (!fs.existsSync(path)) {
+      mkdirSyncRecursive(path);
+    }
+    if (path.substr(-1) !== "/") {
+      path += "/";
+    }
+    this.path = path;
+  }
+  getToken(hashName) {
+    if (fs.existsSync(this.path + hashName)) {
+      return fs.readFileSync(this.path + hashName, { encoding: "utf8" });
+    } else {
+      return "";
+    }
+  }
+  setToken(hashName, token) {
+    fs.writeFileSync(this.path + hashName, token);
+  }
+}
+
+exports.fsProvider = fsProvider;
--- api/memory-provider.js
+++ api/memory-provider.js
@@ -1,14 +1,13 @@
-
 class memoryProvider {
-    constructor() {
-     this.store = {};
-    }
-    getToken(hashName) {
-        return this.store[hashName] || "";
-    }
-    setToken(hashName, token) {
-        return this.store[hashName] = token;
-    }
+  constructor() {
+    this.store = {};
+  }
+  getToken(hashName) {
+    return this.store[hashName] || "";
+  }
+  setToken(hashName, token) {
+    return (this.store[hashName] = token);
+  }
 }
 
-module.exports = memoryProvider;
+exports.memoryProvider = memoryProvider;
