c32d12dae70784d14bd5b9c7c5d77887c9be13ac
Daniyar <dstealord@gmail.com>
2024-11-09 15:02:45 +0500

    export attribute type

--- types/create-contact-data.ts
+++ types/create-contact-data.ts
@@ -10,7 +10,7 @@ export type CreateContactData = {
   attachments?: string[];
 };
 
-type ContactAttribute = {
+export type ContactAttribute = {
   name: string;
   value: string;
   type?: 0 | 1 | 2 | 3 | 4 | 5; // Optional attribute type. Options: 0 - text, 1 - number, 2 - date, 3 - link, 4 - select, 5 - multiSelect.
